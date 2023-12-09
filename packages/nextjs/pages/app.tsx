d63d7d9701d9183b264d8be63872c2457b77e988
mr-harshtyagi <writetoharshtyagi@gmail.com>
2023-12-09 15:07:32 +0530

    feat: add create dataset modal

--- packages/nextjs/pages/app.tsx
+++ packages/nextjs/pages/app.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import CreateDataSet from "~~/components/CreateDataSet";
 import DataSetCard from "~~/components/DataSetCard";
 import { Footer } from "~~/components/Footer";
 import { Header } from "~~/components/Header";
@@ -8,7 +9,17 @@ const App = () => {
     <div>
       <Header />
       {/* My DataSets Heading */}
-      <h2 className="text-4xl text-white font-bold ml-10 mt-8">Owned DataSets</h2>
+      <div className="flex justify-between">
+        <h2 className="text-4xl text-white font-bold ml-10 mt-8">Owned DataSets</h2>
+        <button
+          onClick={() => document.getElementById("create_dataset_modal").showModal()}
+          className="btn btn-active btn-primary rounded-lg mr-20 mt-8 font-semibold text-white hover:scale-105 "
+        >
+          + Create DataSet
+        </button>
+      </div>
+      <CreateDataSet />
+
       <div className=" text-white p-10 grid grid-cols-3 w-full">
         <DataSetCard
           dataSetName="Test"
