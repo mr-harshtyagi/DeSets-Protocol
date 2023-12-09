df0e57f6b4fb077d4a3624f28837b4863a55b233
mr-harshtyagi <writetoharshtyagi@gmail.com>
2023-12-09 10:04:05 +0530

    feat: add state variables and constructor in DataSet contract

--- packages/hardhat/contracts/DataSet.sol
+++ packages/hardhat/contracts/DataSet.sol
@@ -4,4 +4,31 @@ pragma solidity >=0.8.0 <0.9.0;
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
-contract DataSet is ERC721, Ownable {}
+contract DataSet is ERC721, Ownable {
+	// state variables
+	uint256 public price;
+	uint256 public totalSupply;
+	uint256 public maxMintAmount;
+	bool public paused;
+	string private dataSetURI;
+
+	// events
+	event CreateNFT(uint256 indexed id);
+	event Pause(bool isPaused);
+	event ChangePrice(uint256 newPrice);
+	event ChangeMaxMintAmount(uint256 newMaxMintAmount);
+	event ChangeDataSetURI(string newBaseURI);
+
+	// constructor
+	constructor(
+		string memory _name,
+		uint256 _price,
+		string memory _dataSetURI,
+		address _owner
+	) ERC721(_name, "SETS") {
+		price = _price;
+		maxMintAmount = 1;
+		dataSetURI = _dataSetURI;
+		transferOwnership(_owner);
+	}
+}
