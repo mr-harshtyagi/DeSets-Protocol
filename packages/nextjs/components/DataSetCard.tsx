43c5ca3b4a240b9c6820e8f34a50d55b585a500d
mr-harshtyagi <writetoharshtyagi@gmail.com>
2023-12-09 14:21:18 +0530

    feat: add updated dataset card

--- packages/nextjs/components/DataSetCard.tsx
+++ packages/nextjs/components/DataSetCard.tsx
@@ -16,14 +16,16 @@ const DataSetCard = ({
   creatorWallet,
 }: DataSetCardProps) => {
   return (
-    <div className="card w-96 bg-base-100 shadow-xl m-4">
-      <figure className="px-10 pt-10">
-        <img
+    <div className="card w-96 bg-base-100 shadow-xl m-4 hover:scale-105 hover:transition-transform duration-300 ease-in-out">
+      {/* A gradient div */}
+      <div className=" bg-gradient-to-r from-cyan-500 to-blue-500 h-40 rounded-t-xl"></div>
+      {/* The image */}
+      {/* <img
           src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
           alt="Shoes"
           className="rounded-xl"
-        />
-      </figure>
+        /> */}
+
       <div className="card-body items-center text-center text-black">
         <h2 className="card-title">{dataSetName}</h2>
         <p>{dataSetDescription}</p>
