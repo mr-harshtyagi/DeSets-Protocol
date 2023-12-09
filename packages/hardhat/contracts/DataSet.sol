f7b88c96dfb981335f56a06797c6165eedd911d8
mr-harshtyagi <writetoharshtyagi@gmail.com>
2023-12-09 10:01:18 +0530

    feat: add initial DeSetsFactory.sol and DataSet.sol contracts

---
+++ packages/hardhat/contracts/DataSet.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.0 <0.9.0;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract DataSet is ERC721, Ownable {}
---
+++ packages/hardhat/contracts/DeSetsFactory.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity >=0.8.0 <0.9.0;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "./DataSet.sol";
+
+contract DeSetsFactory is Ownable {}
