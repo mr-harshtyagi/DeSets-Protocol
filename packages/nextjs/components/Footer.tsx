a7fbca4dc5f816db12b0778a8dbc7b8dce091b29
mr-harshtyagi <writetoharshtyagi@gmail.com>
2023-12-09 12:16:24 +0530

    feat: add marketplace and update footer component

--- packages/nextjs/components/Footer.tsx
+++ packages/nextjs/components/Footer.tsx
@@ -29,7 +29,7 @@ export const Footer = () => {
             )}
             {isLocalNetwork && (
               <>
-                <Faucet />
+                {/* <Faucet /> */}
                 <Link
                   href="/blockexplorer"
                   passHref
--- packages/nextjs/pages/marketplace.tsx
+++ packages/nextjs/pages/marketplace.tsx
@@ -2,16 +2,16 @@ import React from "react";
 import { Footer } from "~~/components/Footer";
 import { Header } from "~~/components/Header";
 
-const App = () => {
+const Marketplace = () => {
   return (
     <div>
       <Header />
       <div className="container text-white">
-        <h1>App</h1>
+        <h1>Marketplace</h1>
       </div>
       <Footer />
     </div>
   );
 };
 
-export default App;
+export default Marketplace;
